be148b14dc6d64ed6c1dc229ab6539562f9b69d3
BLDhound <lekarrikaila567@gmail.com>
2023-05-03 08:47:03 +0000

    stats

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,7 +1,14 @@
 from api.v1.views import app_views
 from flask import jsonify
+from models import storage
 
 
-@app_views.route('/status')
-def status():
-    return jsonify({'status': 'OK'})
+@app_views.route('/stats')
+def count_objects():
+    counts = {"amenities": storage.count("Amenity"),
+              "cities": storage.count("City"),
+              "places": storage.count("Place"),
+              "reviews": storage.count("Review"),
+              "states": storage.count("State"),
+              "users": storage.count("User")}
+    return jsonify(counts)
